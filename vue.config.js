71f4645ee23154def564a92ee1b28cae04a1bcd5
HongBin2112 <benson890720@gmail.com>
2022-10-09 13:04:28 +0800

    -

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ddhb_ml_website_vue/'
+    : '/'
 
 })
